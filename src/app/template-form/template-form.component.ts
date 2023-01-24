2ff78004ed0cfe3cc29cec41d5b32f8b5965f3c3
Raphael Braga <raffael.info@gmail.com>
2023-01-24 11:10:00 -0300

    aula 86 - sistema de consulta cep em formulario

--- src/app/template-form/template-form.component.ts
+++ src/app/template-form/template-form.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-template-form',
@@ -10,8 +12,53 @@ export class TemplateFormComponent {
     nome: 'Raphael',
     email: 'raffael.info@gmail.com',
   };
+
+  constructor(private httpClient: HttpClient) {}
+
   onSubmit(form: any) {
     console.log(form);
     console.log(this.usuario);
+  }
+
+  consultaCEP(cep: string, form: any) {
+    console.log(cep);
+    //Nova variável "cep" somente com dígitos.
+    cep = cep.replace(/\D/g, '');
+    //Verifica se campo cep possui valor informado.
+    if (cep != '') {
+      var validacep = /^[0-9]{8}$/;
+      if (validacep.test(cep)) {
+        //Consulta o webservice viacep.com.br/
+        this.httpClient
+          .get(`https://viacep.com.br/ws/${cep}/json`)
+          .pipe(map((dados) => dados))
+          .subscribe((dados) => this.popularDadosForm(dados, form));
+      }
+    }
+  }
+  popularDadosForm(dados: any, form: any) {
+    form.setValue({
+      nome: form.value.nome,
+      email: form.value.email,
+      endereco: {
+        cep: dados.cep,
+        numero: '',
+        complemento: dados.complemento,
+        rua: dados.logradouro,
+        bairro: dados.bairro,
+        cidade: dados.localidade,
+        estado: dados.uf,
+      },
+    });
+    form.form.patchValue({
+      endereco: {
+        cep: dados.cep,
+        complemento: dados.complemento,
+        rua: dados.logradouro,
+        bairro: dados.bairro,
+        cidade: dados.localidade,
+        estado: dados.uf,
+      },
+    });
   }
 }
--- src/app/template-form/template-form.module.ts
+++ src/app/template-form/template-form.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { TemplateFormComponent } from './template-form.component';
 import { FormsModule } from '@angular/forms';
 import { FormDebugComponent } from '../form-debug/form-debug.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [TemplateFormComponent, FormDebugComponent],
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, HttpClientModule],
 })
 export class TemplateFormModule {}
