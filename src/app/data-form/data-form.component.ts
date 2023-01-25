d3f140b784f3a531c2f6be65da627cb1d246492f
Raphael Braga <raffael.info@gmail.com>
2023-01-24 21:50:46 -0300

    aula 106 - checkbox toggle

--- src/app/data-form/data-form.component.ts
+++ src/app/data-form/data-form.component.ts
@@ -63,6 +63,7 @@ export class DataFormComponent implements OnInit {
       cargo: [null],
       tecnologias: [null],
       newsletter: ['s'],
+      termos: [null, Validators.pattern('true')],
     });
   }
 
